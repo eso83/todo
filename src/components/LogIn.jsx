6c744f16a534c0a5f9ba44825276d2964191c666
ehsan <eazizi193@gmail.com>
2025-02-06 21:17:27 +0330

    fix Login

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -6,11 +6,13 @@ const LogIn = () => {
     username: "",
     password: "",
   });
-  const handleUserName = (e) => {
-    profileInfo.userName = e.target.value;
-  };
-  const handlePassWord = (e) => {
-    profileInfo.passWord = e.target.value;
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setProfileInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
   const handleLogIn = () => {
     if (profileInfo.password === "admin" && profileInfo.username === "admin") {
@@ -23,7 +25,12 @@ const LogIn = () => {
     <>
       <div className="container">
         <h1>LogIn</h1>
-        <input type="text" placeholder="Enter userName..." />
+        <input
+          type="text"
+          placeholder="Enter username..."
+          name="username"
+          onChange={handleChange}
+        />
         <input
           type="password"
           placeholder="Enter passweord..."
