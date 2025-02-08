2cefbb5b6926a0fe4cc9888e75d3e9285364367e
ehsan <eazizi193@gmail.com>
2025-02-08 20:12:27 +0330

    add uuid to task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,48 +1,52 @@
 import { useContext } from "react";
 import { taskContext } from "./todo";
+import { v4 as uuidv4 } from "uuid";
 const Task = () => {
   const task = useContext(taskContext);
   return (
     <>
-      {task.taskList.map((item, index) => (
-        <div className="taskContainer" key={index}>
-          <p>task:</p>
-          <li>
-            {task.editIndex === index ? (
-              <input
-                className="editInput"
-                type="text"
-                value={task.editTask}
-                onChange={(e) => task.setEditTask(e.target.value)}
-              />
-            ) : (
-              item.task
-            )}
-          </li>
-          <input
-            className="checkBox"
-            type="checkbox"
-            onChange={() => task.handleStatus(index)}
-            checked={item.status}
-          />
-          <button
-            onClick={() => task.handleDelete(index)}
-            className="deleteBtn"
-          >
-            Delete
-          </button>
-          <button
-            onClick={() =>
-              task.editIndex === index
-                ? task.handelSaveTask(index)
-                : task.handleEdite(index)
-            }
-            className="editBtn"
-          >
-            {task.editIndex === index ? "Save" : "Edit"}
-          </button>
-        </div>
-      ))}
+      {task.taskList.map((item, index) => {
+        const uid = uuidv4();
+        return (
+          <div className="taskContainer" key={uid}>
+            <p>task:</p>
+            <li>
+              {task.editIndex === index ? (
+                <input
+                  className="editInput"
+                  type="text"
+                  value={task.editTask}
+                  onChange={(e) => task.setEditTask(e.target.value)}
+                />
+              ) : (
+                item.task
+              )}
+            </li>
+            <input
+              className="checkBox"
+              type="checkbox"
+              onChange={() => task.handleStatus(index)}
+              checked={item.status}
+            />
+            <button
+              onClick={() => task.handleDelete(index)}
+              className="deleteBtn"
+            >
+              Delete
+            </button>
+            <button
+              onClick={() =>
+                task.editIndex === index
+                  ? task.handelSaveTask(index)
+                  : task.handleEdite(index)
+              }
+              className="editBtn"
+            >
+              {task.editIndex === index ? "Save" : "Edit"}
+            </button>
+          </div>
+        );
+      })}
     </>
   );
 };
